24cf00c5c5cd7cb2995b3931f9261ad2f0170256
IsaacAmorteg <isaacamortegc@outlook.com>
2022-11-14 22:04:02 -0500

    Data Structure Homework v1

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -11,9 +11,68 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+class LinkedList {
+  constructor (){
+    this._length = 0;
+    this.head = null;
+  };
 
-function Node(value) {}
+  add(value) {
+    let node = new Node(value);
+    let current = this.head;
+
+    if(!current) {
+      this.head = node;
+      return node;
+    };
+
+    while (current.next) {
+      current = current.next;
+    };
+    current.next = node;
+    return node;
+  };
+
+  remove(){
+    if (!this.head) return null;
+    if (!this.head.next) {
+      let saveInfo = this.head.value;
+      this.head = null;
+      return saveInfo;
+    }
+    else {
+      let current = this.head
+      while (current.next.next){
+        current = current.next;
+      };
+      let saveInfo = current.next.value;
+      current.next = null;
+      return saveInfo;
+    }
+  };
+
+  search (data){
+    let current = this.head;
+    while (current) {
+      if (typeof data !== "function"){
+        if (data === current.value) return current.value;
+      } else {
+        if (data(current.value)) return current.value;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+};
+
+class Node{
+  constructor (value){
+  this.value = value;
+  this.next = null;
+  }
+};
+
+
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +89,34 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+class HashTable {
+  constructor(){
+    this.numBuckets = 35;
+    this.Lockers = [];
+  };
+  hash (key) {
+    let total = 0;
+    for (let i=0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+      };
+      return total % this.numBuckets;
+  };
+  set (key, value) {
+    if (typeof key !== 'string') throw new TypeError('Keys must be strings');
+    let locker = this.hash(key);
+    this.Lockers[locker][key] = value;
+  }; 
+  get (key){
+    let locker = this.hash(key);
+    return this.Lockers[locker][key];
+  };
+
+  hasKey (key){
+  let checkKey = this.get(key)
+  if (checkKey) return true;
+  return false;
+  };
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
